ddc4c430f66e162e30a47aae00afcae8229044b1
scu-heplab <90947124+scu-heplab@users.noreply.github.com>
2025-05-31 00:18:40 +0800

    Add files via upload

---
+++ src/vmamba2.py
@@ -0,0 +1,458 @@
+import math
+import torch
+from typing import Any
+from functools import partial
+from timm.layers import DropPath
+from utils.ssd_minimal import selective_scan_chunk_fn
+from utils.csm_triton import cross_scan_fn, cross_merge_fn
+
+DropPath.__repr__ = lambda self: f"timm.DropPath({self.drop_prob})"
+
+
+class Linear2d(torch.nn.Linear):
+    def forward(self, x: torch.Tensor):
+        # B, C, H, W = x.shape
+        return torch.nn.functional.conv2d(x, self.weight[:, :, None, None], self.bias)
+
+    def _load_from_state_dict(self, state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs):
+        state_dict[prefix + "weight"] = state_dict[prefix + "weight"].view(self.weight.shape)
+        return super()._load_from_state_dict(state_dict, prefix, local_metadata, strict, missing_keys, unexpected_keys, error_msgs)
+
+
+class LayerNorm2d(torch.nn.LayerNorm):
+    def forward(self, x: torch.Tensor):
+        x = x.permute(0, 2, 3, 1)
+        x = torch.nn.functional.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        x = x.permute(0, 3, 1, 2)
+        return x
+
+
+class Permute(torch.nn.Module):
+    def __init__(self, *args):
+        super().__init__()
+        self.args = args
+
+    def forward(self, x: torch.Tensor):
+        return x.permute(*self.args).contiguous()
+
+
+class Mlp(torch.nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=torch.nn.GELU, drop=0., channels_first=False):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+
+        Linear = Linear2d if channels_first else torch.nn.Linear
+        self.fc1 = Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = Linear(hidden_features, out_features)
+        self.drop = torch.nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class gMlp(torch.nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=torch.nn.GELU, drop=0., channels_first=False):
+        super().__init__()
+        self.channel_first = channels_first
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+
+        Linear = Linear2d if channels_first else torch.nn.Linear
+        self.fc1 = Linear(in_features, 2 * hidden_features)
+        self.act = act_layer()
+        self.fc2 = Linear(hidden_features, out_features)
+        self.drop = torch.nn.Dropout(drop)
+
+    def forward(self, x: torch.Tensor):
+        x = self.fc1(x)
+        x, z = x.chunk(2, dim=(1 if self.channel_first else -1))
+        x = self.fc2(x * self.act(z))
+        x = self.drop(x)
+        return x
+
+
+class SoftmaxSpatial(torch.nn.Softmax):
+    def forward(self, x: torch.Tensor):
+        if self.dim == -1:
+            B, C, H, W = x.shape
+            return super().forward(x.view(B, C, -1)).view(B, C, H, W)
+        elif self.dim == 1:
+            B, H, W, C = x.shape
+            return super().forward(x.view(B, -1, C)).view(B, H, W, C)
+        else:
+            raise NotImplementedError
+
+
+class SS2Dm0:
+    def __initm0__(
+            self,
+            # basic dims ===========
+            d_model=96,
+            d_state=16,  # now with mamba2, dstate should be bigger...
+            ssm_ratio=2.0,
+            dt_rank="auto",
+            act_layer=torch.nn.GELU,
+            # dwconv ===============
+            d_conv=3,  # < 2 means no conv
+            conv_bias=True,
+            # ======================
+            dropout=0.0,
+            bias=False,
+            # dt init ==============
+            dt_min=0.001,
+            dt_max=0.1,
+            dt_init="random",
+            dt_scale=1.0,
+            dt_init_floor=1e-4,
+            initialize="v2",
+            # ======================
+            forward_type="m0",
+            # ======================
+            with_initial_state=False,
+            # ======================
+            **kwargs,
+    ):
+        factory_kwargs = {"device": None, "dtype": None}
+        super().__init__()
+        d_inner = int(ssm_ratio * d_model)
+        dt_rank = math.ceil(d_model / 16) if dt_rank == "auto" else dt_rank
+        assert d_inner % dt_rank == 0
+        self.with_dconv = d_conv > 1
+        Linear = torch.nn.Linear
+        self.forward = self.forwardm0
+
+        # tags for forward_type ==============================
+        self.disable_force32, forward_type = self.checkpostfix("_no32", forward_type)
+        self.oact, forward_type = self.checkpostfix("_oact", forward_type)
+        self.disable_z, forward_type = self.checkpostfix("_noz", forward_type)
+        self.disable_z_act, forward_type = self.checkpostfix("_nozact", forward_type)
+        self.out_norm, forward_type = self.get_outnorm(forward_type, d_inner, False)
+
+        # forward_type debug =======================================
+        FORWARD_TYPES = dict(
+            m0=partial(self.forward_corem0, force_fp32=False, dstate=d_state),
+        )
+        self.forward_core = FORWARD_TYPES.get(forward_type, None)
+        k_group = 4
+
+        # in proj =======================================
+        d_proj = d_inner if self.disable_z else (d_inner * 2)
+        self.in_proj = Linear(d_model, d_proj, bias=bias)
+        self.act: torch.nn.Module = act_layer()
+
+        # conv =======================================
+        if self.with_dconv:
+            self.conv2d = torch.nn.Sequential(
+                Permute(0, 3, 1, 2),
+                torch.nn.Conv2d(
+                    in_channels=d_inner,
+                    out_channels=d_inner,
+                    groups=d_inner,
+                    bias=conv_bias,
+                    kernel_size=d_conv,
+                    padding=(d_conv - 1) // 2,
+                    **factory_kwargs,
+                ),
+                Permute(0, 2, 3, 1),
+            )
+
+            # x proj ============================
+        self.x_proj = [
+            torch.nn.Linear(d_inner, (dt_rank + d_state * 2), bias=False)
+            for _ in range(k_group)
+        ]
+        self.x_proj_weight = torch.nn.Parameter(torch.stack([t.weight for t in self.x_proj], dim=0))  # (K, N, inner)
+        del self.x_proj
+
+        # out proj =======================================
+        self.out_act = torch.nn.GELU() if self.oact else torch.nn.Identity()
+        self.out_proj = Linear(d_inner, d_model, bias=bias)
+        self.dropout = torch.nn.Dropout(dropout) if dropout > 0. else torch.nn.Identity()
+
+        if initialize in ["v1"]:
+            # simple init dt_projs, A_logs, Ds
+            self.Ds = torch.nn.Parameter(torch.ones((k_group, dt_rank, int(d_inner // dt_rank))))
+            self.A_logs = torch.nn.Parameter(torch.randn((k_group, dt_rank)))  # A == -A_logs.exp() < 0; # 0 < exp(A * dt) < 1
+            self.dt_projs_bias = torch.nn.Parameter(0.1 * torch.randn((k_group, dt_rank)))  # 0.1 is added in 0430
+        elif initialize in ["v2"]:
+            # simple init dt_projs, A_logs, Ds
+            self.Ds = torch.nn.Parameter(torch.ones((k_group, dt_rank, int(d_inner // dt_rank))))
+            self.A_logs = torch.nn.Parameter(torch.zeros((k_group, dt_rank)))  # A == -A_logs.exp() < 0; # 0 < exp(A * dt) < 1
+            self.dt_projs_bias = torch.nn.Parameter(0.1 * torch.rand((k_group, dt_rank)))
+
+        # init state ============================
+        self.initial_state = None
+        if with_initial_state:
+            self.initial_state = torch.nn.Parameter(torch.zeros((1, k_group * dt_rank, int(d_inner // dt_rank), d_state)), requires_grad=False)
+
+    def forward_corem0(
+            self,
+            x: torch.Tensor = None,
+            # ==============================
+            force_fp32=False,  # True: input fp32
+            chunk_size=64,
+            dstate=64,
+            # ==============================
+            selective_scan_backend=None,
+            scan_mode="cross2d",
+            scan_force_torch=False,
+            # ==============================
+            **kwargs,
+    ):
+        assert scan_mode in ["unidi", "bidi", "cross2d"]
+        assert selective_scan_backend in [None, "triton", "torch"]
+        x_proj_bias = getattr(self, "x_proj_bias", None)
+        to_fp32 = lambda *args: (_a.to(torch.float32) for _a in args)
+
+        N = dstate
+        B, H, W, RD = x.shape
+        K, R = self.A_logs.shape
+        K, R, D = self.Ds.shape
+        assert RD == R * D
+        L = H * W
+        KR = K * R
+        _scan_mode = dict(cross2d=0, unidi=1, bidi=2, cascade2d=3)[scan_mode]
+
+        initial_state = None
+        if self.initial_state is not None:
+            assert self.initial_state.shape[-1] == dstate
+            initial_state = self.initial_state.detach().repeat(B, 1, 1, 1)
+        xs = cross_scan_fn(x.view(B, H, W, RD), in_channel_first=False, out_channel_first=False, scans=_scan_mode, force_torch=scan_force_torch)  # (B, H, W, 4, D)
+        x_dbl = torch.einsum("b l k d, k c d -> b l k c", xs, self.x_proj_weight)
+        if x_proj_bias is not None:
+            x_dbl = x_dbl + x_proj_bias.view(1, -1, K, 1)
+        dts, Bs, Cs = torch.split(x_dbl, [R, N, N], dim=3)
+        xs = xs.contiguous().view(B, L, KR, D)
+        dts = dts.contiguous().view(B, L, KR)
+        Bs = Bs.contiguous().view(B, L, K, N)
+        Cs = Cs.contiguous().view(B, L, K, N)
+        if force_fp32:
+            xs, dts, Bs, Cs = to_fp32(xs, dts, Bs, Cs)
+
+        As = -self.A_logs.to(torch.float).exp().view(KR)
+        Ds = self.Ds.to(torch.float).view(KR, D)
+        dt_bias = self.dt_projs_bias.view(KR)
+
+        if force_fp32:
+            xs, dts, Bs, Cs = to_fp32(xs, dts, Bs, Cs)
+
+        ys, final_state = selective_scan_chunk_fn(
+            xs, dts, As, Bs, Cs, chunk_size=chunk_size, D=Ds, dt_bias=dt_bias,
+            initial_states=initial_state, dt_softplus=True, return_final_states=True,
+            backend=selective_scan_backend,
+        )
+        y: torch.Tensor = cross_merge_fn(ys.view(B, H, W, K, RD), in_channel_first=False, out_channel_first=False, scans=_scan_mode, force_torch=scan_force_torch)
+
+        if getattr(self, "__DEBUG__", False):
+            setattr(self, "__data__", dict(A_logs=self.A_logs, Bs=Bs, Cs=Cs, Ds=self.Ds, us=xs, dts=dts, delta_bias=self.dt_projs_bias,
+                                           initial_state=self.initial_state, final_satte=final_state, ys=ys, y=y, H=H, W=W))
+        if self.initial_state is not None:
+            self.initial_state = torch.nn.Parameter(final_state.detach().sum(0, keepdim=True), requires_grad=False)
+
+        y = self.out_norm(y.view(B, H, W, -1))
+
+        return y.to(x.dtype)
+
+    def forwardm0(self, x: torch.Tensor, **kwargs):
+        x = self.in_proj(x)
+        if not self.disable_z:
+            x, z = x.chunk(2, dim=(1 if self.channel_first else -1))  # (b, h, w, d)
+            if not self.disable_z_act:
+                z = self.act(z)
+        if self.with_dconv:
+            x = self.conv2d(x)  # (b, d, h, w)
+        x = self.act(x)
+        y = self.forward_core(x)
+        y = self.out_act(y)
+        if not self.disable_z:
+            y = y * z
+        out = self.dropout(self.out_proj(y))
+        return out
+
+    @staticmethod
+    def get_outnorm(forward_type="", d_inner=192, channel_first=True):
+        def checkpostfix(tag, value):
+            ret = value[-len(tag):] == tag
+            if ret:
+                value = value[:-len(tag)]
+            return ret, value
+
+        LayerNorm = LayerNorm2d if channel_first else torch.nn.LayerNorm
+
+        out_norm_none, forward_type = checkpostfix("_onnone", forward_type)
+        out_norm_dwconv3, forward_type = checkpostfix("_ondwconv3", forward_type)
+        out_norm_cnorm, forward_type = checkpostfix("_oncnorm", forward_type)
+        out_norm_softmax, forward_type = checkpostfix("_onsoftmax", forward_type)
+        out_norm_sigmoid, forward_type = checkpostfix("_onsigmoid", forward_type)
+
+        out_norm = torch.nn.Identity()
+        if out_norm_none:
+            out_norm = torch.nn.Identity()
+        elif out_norm_cnorm:
+            out_norm = torch.nn.Sequential(
+                LayerNorm(d_inner),
+                (torch.nn.Identity() if channel_first else Permute(0, 3, 1, 2)),
+                torch.nn.Conv2d(d_inner, d_inner, kernel_size=3, padding=1, groups=d_inner, bias=False),
+                (torch.nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            )
+        elif out_norm_dwconv3:
+            out_norm = torch.nn.Sequential(
+                (torch.nn.Identity() if channel_first else Permute(0, 3, 1, 2)),
+                torch.nn.Conv2d(d_inner, d_inner, kernel_size=3, padding=1, groups=d_inner, bias=False),
+                (torch.nn.Identity() if channel_first else Permute(0, 2, 3, 1)),
+            )
+        elif out_norm_softmax:
+            out_norm = SoftmaxSpatial(dim=(-1 if channel_first else 1))
+        elif out_norm_sigmoid:
+            out_norm = torch.nn.Sigmoid()
+        else:
+            out_norm = LayerNorm(d_inner)
+
+        return out_norm, forward_type
+
+    @staticmethod
+    def checkpostfix(tag, value):
+        ret = value[-len(tag):] == tag
+        if ret:
+            value = value[:-len(tag)]
+        return ret, value
+
+
+class SS2D(torch.nn.Module, SS2Dm0):
+    def __init__(
+            self,
+            # basic dims ===========
+            d_model=96,
+            d_state=16,
+            ssm_ratio=2.0,
+            dt_rank="auto",
+            act_layer=torch.nn.SiLU,
+            # dwconv ===============
+            d_conv=3,  # < 2 means no conv
+            conv_bias=True,
+            # ======================
+            dropout=0.0,
+            bias=False,
+            # dt init ==============
+            dt_min=0.001,
+            dt_max=0.1,
+            dt_init="random",
+            dt_scale=1.0,
+            dt_init_floor=1e-4,
+            initialize="v0",
+            # ======================
+            forward_type="v2",
+            channel_first=False,
+            # ======================
+            **kwargs,
+    ):
+        torch.nn.Module.__init__(self)
+        kwargs.update(
+            d_model=d_model, d_state=d_state, ssm_ratio=ssm_ratio, dt_rank=dt_rank,
+            act_layer=act_layer, d_conv=d_conv, conv_bias=conv_bias, dropout=dropout, bias=bias,
+            dt_min=dt_min, dt_max=dt_max, dt_init=dt_init, dt_scale=dt_scale, dt_init_floor=dt_init_floor,
+            initialize=initialize, forward_type=forward_type, channel_first=channel_first,
+        )
+        if forward_type in ["v0", "v0seq"]:
+            self.__initv0__(seq=("seq" in forward_type), **kwargs)
+        elif forward_type.startswith("xv"):
+            self.__initxv__(**kwargs)
+        elif forward_type.startswith("m"):
+            self.__initm0__(**kwargs)
+        else:
+            self.__initv2__(**kwargs)
+
+
+# =====================================================
+class VSSBlock(torch.nn.Module):
+    def __init__(
+            self,
+            hidden_dim: int = 0,
+            drop_path: float = 0.2,
+            norm_layer: torch.nn.Module = torch.nn.LayerNorm,
+            channel_first=False,
+            # =============================
+            ssm_d_state: int = 64,
+            ssm_ratio=1.0,
+            ssm_dt_rank: Any = "auto",
+            ssm_act_layer=torch.nn.GELU,
+            ssm_conv: int = 3,
+            ssm_conv_bias=False,
+            ssm_drop_rate: float = 0,
+            ssm_init="v2",
+            forward_type="m0_noz",
+            # =============================
+            mlp_ratio=4.0,
+            mlp_act_layer=torch.nn.GELU,
+            mlp_drop_rate: float = 0.0,
+            gmlp=False,
+            # =============================
+            use_checkpoint: bool = False,
+            post_norm: bool = False,
+            # =============================
+            _SS2D: type = SS2D,
+            **kwargs,
+    ):
+        super().__init__()
+        self.ssm_branch = ssm_ratio > 0
+        self.mlp_branch = mlp_ratio > 0
+        self.use_checkpoint = use_checkpoint
+        self.post_norm = post_norm
+
+        if self.ssm_branch:
+            self.norm = norm_layer(hidden_dim)
+            self.op = _SS2D(
+                d_model=hidden_dim,
+                d_state=ssm_d_state,
+                ssm_ratio=ssm_ratio,
+                dt_rank=ssm_dt_rank,
+                act_layer=ssm_act_layer,
+                # ==========================
+                d_conv=ssm_conv,
+                conv_bias=ssm_conv_bias,
+                # ==========================
+                dropout=ssm_drop_rate,
+                # bias=False,
+                # ==========================
+                # dt_min=0.001,
+                # dt_max=0.1,
+                # dt_init="random",
+                # dt_scale="random",
+                # dt_init_floor=1e-4,
+                initialize=ssm_init,
+                # ==========================
+                forward_type=forward_type,
+                channel_first=channel_first,
+            )
+
+        self.drop_path = DropPath(drop_path)
+
+        if self.mlp_branch:
+            _MLP = Mlp if not gmlp else gMlp
+            self.norm2 = norm_layer(hidden_dim)
+            mlp_hidden_dim = int(hidden_dim * mlp_ratio)
+            self.mlp = _MLP(in_features=hidden_dim, hidden_features=mlp_hidden_dim, act_layer=mlp_act_layer, drop=mlp_drop_rate, channels_first=channel_first)
+
+    def _forward(self, inputs: torch.Tensor):
+        x = inputs
+        if self.ssm_branch:
+            if self.post_norm:
+                x = x + self.drop_path(self.norm(self.op(x)))
+            else:
+                x = x + self.drop_path(self.op(self.norm(x)))
+        if self.mlp_branch:
+            if self.post_norm:
+                x = x + self.drop_path(self.norm2(self.mlp(x)))  # FFN
+            else:
+                x = x + self.drop_path(self.mlp(self.norm2(x)))  # FFN
+        return x
+
+    def forward(self, inputs: torch.Tensor):
+        return self._forward(inputs)
